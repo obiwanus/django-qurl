952cebb21536486ec6488981ed8ae4f7bf0834ff
Ivan Ivanov <ivan.ivanov@yellow.co.nz>
2014-01-23 16:15:14 +1300

    Test config

--- qurl/tests.py
+++ qurl/tests.py
@@ -1,26 +1,26 @@
 from django.template import Template, Context
 
 
-def test_qurl_append(self):
+def test_qurl_append():
     out = Template(
         '{% load qurl %}'
         '{% qurl "http://example.com/?a=1" a+="2" a-="1" %}'
     ).render(Context())
-    self.assertEqual(out, 'http://example.com/?a=2')
+    assert out == 'http://example.com/?a=2'
 
 
-def test_qurl_set(self):
+def test_qurl_set():
     out = Template(
         '{% load qurl %}'
         '{% qurl "http://example.com/?a=1" a=None b="1" %}'
     ).render(Context())
-    self.assertEqual(out, 'http://example.com/?b=1')
+    assert out == 'http://example.com/?b=1'
 
 
-def test_qurl_as(self):
+def test_qurl_as():
     context = Context()
     Template(
         '{% load qurl %}'
         '{% qurl "http://example.com/?a=1" a=None as url %}'
     ).render(context)
-    self.assertEqual(context.get('url'), 'http://example.com/')
+    assert context.get('url') == 'http://example.com/'
