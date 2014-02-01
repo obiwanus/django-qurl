dd4caa02e54af11dad217fa106c8b7a44543c55a
Ivan Ivanov <ivan@ivanovs.info>
2014-02-01 17:57:46 +1300

    A failing test for the future reverse

--- tests/tests.py
+++ tests/tests.py
@@ -1,4 +1,7 @@
+from __future__ import unicode_literals
+
 import os
+import pytest
 from django.template import Template, Context
 
 
@@ -8,7 +11,7 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
 def test_append():
     out = Template(
         "{% load qurl %}"
-        "{% qurl 'http://example.com/?a=1&b=3' a+=2 a-=1 %}"
+        "{% qurl '/testurl/?a=1&b=3' a+=2 a-=1 %}"
     ).render(Context())
     assert 'b=3' in out
     assert 'a=2' in out
@@ -17,14 +20,14 @@ def test_append():
 def test_set():
     out = Template(
         "{% load qurl %}"
-        "{% qurl 'http://example.com/?a=1&b=2' b=1 %}"
+        "{% qurl '/testurl/?a=1&b=2' b=1 %}"
     ).render(Context())
     assert 'b=2' not in out
     assert 'b=1' in out
     assert 'a=1' in out
 
 
-def test_qurl_as():
+def test_as_context_var():
     context = Context()
     template = Template(
         "{% load qurl %}"
@@ -40,3 +43,12 @@ def test_quotes_in_params():
         "{% qurl 'http://example.com/?a=1&b=2' b='vodka vodka' %}"
     ).render(Context())
     assert out == 'http://example.com/?a=1&b=vodka+vodka'
+
+
+@pytest.mark.xfail
+def test_reverse():
+    out = Template(
+        "{% load qurl %}"
+        "{% qurl 'testurl' 12 | a=3 b=5 %}"
+    ).render(Context())
+    assert out == '/testurl/12/?a=3&b=5'
