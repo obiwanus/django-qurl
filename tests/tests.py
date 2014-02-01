9b9aea7f6981d1bcb535ae3dac9eab964adc8310
Ivan Ivanov <ivan@ivanovs.info>
2014-02-01 17:09:39 +1300

    Modified the tests

--- tests/tests.py
+++ tests/tests.py
@@ -5,26 +5,30 @@ from django.template import Template, Context
 os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
 
 
-def test_qurl_append():
+def test_append():
     out = Template(
-        '{% load qurl %}'
-        '{% qurl "http://example.com/?a=1" a+="2" a-="1" %}'
+        "{% load qurl %}"
+        "{% qurl 'http://example.com/?a=1&b=3' a+=2 a-=1 %}"
     ).render(Context())
-    assert out == 'http://example.com/?a=2'
+    assert 'b=3' in out
+    assert 'a=2' in out
 
 
-def test_qurl_set():
+def test_set():
     out = Template(
-        '{% load qurl %}'
-        '{% qurl "http://example.com/?a=1&a=2" a-- b="1" %}'
+        "{% load qurl %}"
+        "{% qurl 'http://example.com/?a=1&b=2' b=1 %}"
     ).render(Context())
-    assert out == 'http://example.com/?b=1'
+    assert 'b=2' not in out
+    assert 'b=1' in out
+    assert 'a=1' in out
 
 
 def test_qurl_as():
     context = Context()
-    Template(
-        '{% load qurl %}'
-        '{% qurl "http://example.com/?a=1" a-- as url %}'
-    ).render(context)
-    assert context.get('url') == 'http://example.com/'
+    template = Template(
+        "{% load qurl %}"
+        "{% qurl 'http://example.com/?a=1' a-- as url1 %}"
+        "-{{ url1 }}-"
+    )
+    assert template.render(context) == '-http://example.com/-'
